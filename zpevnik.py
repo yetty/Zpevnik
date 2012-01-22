cb7d68506f726dba6a63c0b6f4288665cc3a7439
Juda Kaleta <juda.kaleta@gmail.com>
2012-01-22 21:17:08 +0100

    Zpevnik init

---
+++ zpevnik.py
@@ -0,0 +1,76 @@
+import sys, os, re, locale
+
+locale.setlocale(locale.LC_ALL, "")
+
+LYRICS_DIR = './pisne/'
+DATA_DIR = './data/'
+
+
+class Output:
+	out = ''
+	songs = []
+
+	def __init__(self, songs):
+		self.songs = songs
+
+		out = open(DATA_DIR+'template.tex').read()
+		out = out.replace("%CONTENT%", self.getContent())
+
+		self.out = out
+
+	def getContent(self):
+		ret = ''
+		template = open(DATA_DIR+'/song.tex').read()
+
+		for song in self.songs:
+			cret = template.replace("%NAME%", song.name)
+			cret = cret.replace("%LYRICS%", song.lyrics)
+			ret += cret
+
+		return ret
+
+	def get(self):
+		return self.out
+
+
+class Lyrics:
+	name = ''
+	lyrics = ''
+
+	def __init__(self, content):
+		self.name = content.readline().strip()
+		content.readline()
+
+		l = content.read()
+		l = l.replace("#", "\#")
+		l = l.replace("[", "\ch{")
+		l = l.replace("]", "}")
+		l = l.replace("\n", "\\\\\n")
+
+		self.lyrics = l
+
+
+	def get(self):
+		return self.name
+
+
+
+
+if __name__=="__main__":
+	songs = []
+	for file in os.listdir(LYRICS_DIR):
+		filePath = LYRICS_DIR+'/'+file
+		print file
+
+		if os.path.isfile(filePath) and file.split('.')[1]=="txt":
+			content = open(filePath)
+			songs.append(Lyrics(content))
+
+	songs.sort(key=lambda song: song.name, cmp=locale.strcoll)
+
+	for song in songs:
+		print song.get()
+
+	save = open('./output/zpevnik.tex', 'w')
+	save.write(Output(songs).get())
+
